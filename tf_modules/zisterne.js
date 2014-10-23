2942994b5ca83669a9e410311ac70da24377060e
Raphael Vogel <raphael.vogel@web.de>
2014-10-23 20:34:36 +0200

    Set timeout to 6s

--- tf_modules/zisterne.js
+++ tf_modules/zisterne.js
@@ -4,6 +4,7 @@ var PORT = 4223;
 
 exports.readTemperature = function(cb){
     var ipcon = new TF.IPConnection();
+    ipcon.setTimeout(6000);
     var masterBrick = new TF.BrickMaster('6esCZX', ipcon);
     var tempBricklet = new TF.BrickletTemperature('dCf', ipcon);
     tempBricklet.setI2CMode(TF.BrickletTemperature.I2C_MODE_SLOW); // to prevent outlier
@@ -30,6 +31,7 @@ exports.readTemperature = function(cb){
 
 exports.readWaterLevel = function(cb){
     var ipcon = new TF.IPConnection();
+    ipcon.setTimeout(6000);
     var masterBrick = new TF.BrickMaster('6esCZX', ipcon);
     var distUSBricklet = new TF.BrickletDistanceUS('nY3', ipcon);
     distUSBricklet.setMovingAverage(100);
