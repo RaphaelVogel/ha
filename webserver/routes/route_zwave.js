c47169d5ac58b494d5897dfcb50399a1a361c6b4
Raphael Vogel <raphael.vogel@web.de>
2014-11-18 22:50:44 +0100

    Implement zwave handling
    Add ON OFF for binary switch and
    get the status of thebinary switch

--- webserver/routes/route_zwave.js
+++ webserver/routes/route_zwave.js
@@ -1,65 +1,65 @@
+var request = require('request');
 var express = require('express');
 var router = express.Router();
 
-var livingroomLightRoute = router.route('/livingroomLight/:state');
-
-// Temperature API
-livingroomLightRoute.get(function(req, res){
+var livingroomLightSet = router.route('/livingroomLight/:state');
+livingroomLightSet.get(function(req, res){
+    if(global.fake === true){
+        res.status(200).send({"livingroomLight": req.param('state')});
+        return;
+	}
+    
     if(req.param('state') === "ON"){
-        //http://raspi1:8083/ZWaveAPI/Run/devices[2].instances[1].SwitchBinary.Set(255)
-        request.post({url: 'http://haserver:8083/ZWaveAPI/Run/devices[2].instances[1].SwitchBinary.Set(255)'}, 
+        request.post({url: 'http://haserver:8083/ZWaveAPI/Run/devices[2].instances[0].SwitchBinary.Set(255)'}, 
             function(error, response, body){
-                
+                if (error){
+                    res.status(500).send({ "ERROR" : error });
+                }
+                else{
+                    res.status(200).send({"livingroomLight":"ON"});
+                }
             }
         );
     }
     else if(req.param('state') === "OFF"){
-        request.post({url: 'http://haserver:8083/ZWaveAPI/Run/devices[2].instances[1].SwitchBinary.Set(0)'}, 
+        request.post({url: 'http://haserver:8083/ZWaveAPI/Run/devices[2].instances[0].SwitchBinary.Set(0)'}, 
             function(error, response, body){
-                console.log(body);
+                if (error){
+                    res.status(500).send({ "ERROR" : error });
+                }
+                else {
+                   res.status(200).send({"livingroomLight":"OFF"}); 
+                }
             }
         );
     }
 });
 
+var livingroomLightGet = router.route('/livingroomLight');
+livingroomLightGet.get(function(req, res){
+    if(global.fake === true){
+        res.status(200).send({"livingroomLight":"ON"});
+        return;
+	}
+    
+    request.post({url: 'http://haserver:8083/ZWaveAPI/Run/devices[2].instances[0].SwitchBinary.data.level.value'}, 
+        function(error, response, body){
+            if (error){
+                res.status(500).send({ "ERROR" : error });
+            }
+            else {
+                if(body == 'true'){
+                    res.status(200).send({"livingroomLight":"ON"}); 
+                }
+                else {
+                    res.status(200).send({"livingroomLight":"OFF"});
+                }
+
+            }
+        }
+    );
+
+
+});
+
 module.exports = router;
-
-/*
-ON
-{
-    "devices.2.data.lastReceived": {
-      "name": "lastReceived",
-      "value": 0,
-      "type": "int",
-      "invalidateTime": 1415390696,
-      "updateTime": 1415393915
-    },
-    "devices.2.instances.0.commandClasses.37.data.level": {
-      "name": "level",
-      "value": true,
-      "type": "bool",
-      "invalidateTime": 1415393766,
-      "updateTime": 1415393915
-    },
-  "updateTime": 1415393917
-}
-
-OFF
-{
-    "devices.2.data.lastReceived": {
-      "name": "lastReceived",
-      "value": 0,
-      "type": "int",
-      "invalidateTime": 1415390696,
-      "updateTime": 1415394083
-    },
-    "devices.2.instances.0.commandClasses.37.data.level": {
-      "name": "level",
-      "value": false,
-      "type": "bool",
-      "invalidateTime": 1415393766,
-      "updateTime": 1415394083
-    },
-  "updateTime": 1415394085
-}
-*/
