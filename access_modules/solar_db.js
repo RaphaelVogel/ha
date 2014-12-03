7e3346b40eee7b442e666c7ed75d287cd236e864
Raphael Vogel <raphael.vogel@web.de>
2014-12-03 19:55:10 +0100

    fix: create db every time

--- access_modules/solar_db.js
+++ access_modules/solar_db.js
@@ -1,11 +1,12 @@
 var logger = require('../support/logger');
 var sqlite3 = require('sqlite3');
-var db = new sqlite3.Database('../ha.db');
+
 
 var solarData = { "days": []};
 
 exports.readProductionPerDay = function(range, callback){
 	if(!range) range =  1 //month
+    var db = new sqlite3.Database('../ha.db');
 	var sqlString = "select timestamp, val_real from sensordata where device_id = 2 and sensor_id = 1 and datetime(timestamp, '+"+range+" months') >= date('now') order by timestamp asc"; //
 	logger.verbose("Executed SQL statement to read solar data: "+sqlString);
 	db.each(sqlString, function(err, row){
