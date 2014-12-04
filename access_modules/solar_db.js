f954aa504de18aecb942f1d61cee8f5e702bb4cb
Raphael Vogel <raphael.vogel@web.de>
2014-12-04 22:32:32 +0100

    make variable private

--- access_modules/solar_db.js
+++ access_modules/solar_db.js
@@ -1,10 +1,8 @@
 var logger = require('../support/logger');
 var sqlite3 = require('sqlite3');
 
-
-var solarData = { "days": []};
-
 exports.readProductionPerDay = function(range, callback){
+    var solarData = { "days": []};
 	if(!range) range =  1 //month
     var db = new sqlite3.Database('../ha.db');
 	var sqlString = "select timestamp, val_real from sensordata where device_id = 2 and sensor_id = 1 and datetime(timestamp, '+"+range+" months') >= date('now') order by timestamp asc"; //
