9fcdb2647b2c5c07d324807018101e4c62edf9b8
Raphael Vogel <raphael.vogel@web.de>
2014-12-04 11:18:14 +0100

    same

--- webserver/haui/area/weather/WeatherPressureChart.controller.js
+++ webserver/haui/area/weather/WeatherPressureChart.controller.js
@@ -2,7 +2,7 @@ sap.ui.controller("haui.area.weather.WeatherPressureChart", {
     onInit: function() {
         var pressureChartModel = new sap.ui.model.json.JSONModel();
         this.getView().byId("pressureChart").setModel(pressureChartModel);
-        this.getView().byId("pressureChart").setValueAxis(new sap.makit.ValueAxis({min: "900", max: "1100"}));  
+        this.getView().byId("pressureChart").setValueAxis(new sap.makit.ValueAxis({min: "850", max: "1100"}));  
         this.getView().addEventDelegate({
             // call every time when page is displayed
             onBeforeShow: function(evt) {
