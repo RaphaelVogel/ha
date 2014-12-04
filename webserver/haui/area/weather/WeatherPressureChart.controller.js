f35de20e2d555ea981850734abe4cc8a9ec6507a
Raphael Vogel <raphael.vogel@web.de>
2014-12-04 11:13:05 +0100

    min max pressure

--- webserver/haui/area/weather/WeatherPressureChart.controller.js
+++ webserver/haui/area/weather/WeatherPressureChart.controller.js
@@ -2,7 +2,7 @@ sap.ui.controller("haui.area.weather.WeatherPressureChart", {
     onInit: function() {
         var pressureChartModel = new sap.ui.model.json.JSONModel();
         this.getView().byId("pressureChart").setModel(pressureChartModel);
-        this.getView().byId("pressureChart").setValueAxis(new sap.makit.ValueAxis({min: "700", max: "1200"}));  
+        this.getView().byId("pressureChart").setValueAxis(new sap.makit.ValueAxis({min: "800", max: "1100"}));  
         this.getView().addEventDelegate({
             // call every time when page is displayed
             onBeforeShow: function(evt) {
